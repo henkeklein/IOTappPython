1387c4f770d988f1eb765494fbbc5a07638b28a7
Henrik Klein <henrik.klein12@gmail.com>
2016-12-19 15:36:25 +0100

    new

--- server.py
+++ server.py
@@ -1,88 +1,74 @@
-import os
-import json
-import keystoneclient.v3 as keystoneclient
-import swiftclient.client as swiftclient
+import time
+import sys
+import pprint
+import uuid
+
+
 try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
+	import ibmiotf.application
+	import ibmiotf.device
 except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+	# This part is only required to run the sample from within the samples
+	# directory when the module itself is not installed.
+	#
+	# If you have the module installed, just use "import ibmiotf.application" & "import ibmiotf.device"
+	import os
+	import inspect
+	cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../../src")))
+	if cmd_subfolder not in sys.path:
+		sys.path.insert(0, cmd_subfolder)
+	import ibmiotf.application
+	import ibmiotf.device
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('VCAP_APP_PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
 
-def swift():
+def myAppEventCallback(event):
+	print("Received live data from %s (%s) sent at %s: hello=%s x=%s" % (event.deviceId, event.deviceType, event.timestamp.strftime("%H:%M:%S"), data['hello'], data['x']))
 
-    cloudant_service = json.loads(os.environ['VCAP_SERVICES'])['Object-Storage'][0]
-    objectstorage_creds = cloudant_service['credentials']
 
-    if objectstorage_creds:
-        auth_url = objectstorage_creds['auth_url'] + '/v3'
-        project_name = objectstorage_creds['project']
-        password = objectstorage_creds['password']
-        user_domain_name = objectstorage_creds['domainName']
-        project_id = objectstorage_creds['projectId']
-        user_id = objectstorage_creds['userId']
-        region_name = objectstorage_creds['region']
+organization = "quickstart"
+deviceType = "helloWorldDevice"
+deviceId = str(uuid.uuid4())
+appId = deviceId + "_receiver"
+authMethod = None
+authToken = None
 
-    # Get a Swift client connection object
-    conn = swiftclient.Connection(
-            key=password,
-            authurl=auth_url,
-            auth_version='3',
-            os_options={"project_id": project_id,
-                        "user_id": user_id,
-                        "region_name": region_name})
 
-    container_name = 'new-container'
-
-    # File name for testing
-    file_name = 'example_file.txt'
-
-    # Create a new container
-    conn.put_container(container_name)
-    print "\nContainer %s created successfully." % container_name
-
-    # List your containers
-    print ("\nContainer List:")
-    for container in conn.get_account()[1]:
-        print container['name']
-
-    # Create a file for uploading
-    with open(file_name, 'w') as example_file:
-        conn.put_object(container_name,
-        file_name,
-        contents= "",
-        content_type='text/plain')
-
-    # List objects in a container, and prints out each object name, the file size, and last modified date
-    print ("\nObject List:")
-    for container in conn.get_account()[1]:
-        for data in conn.get_container(container['name'])[1]:
-            print 'object: {0}\t size: {1}\t date: {2}'.format(data['name'], data['bytes'], data['last_modified'])
-
-    # Download an object and save it to ./my_example.txt
-    obj = conn.get_object(container_name, file_name)
-    with open(file_name, 'w') as my_example:
-           my_example.write(obj[1])
-           print "\nObject %s downloaded successfully." % file_name
-
-    # Delete an object
-    conn.delete_object(container_name, file_name)
-    print "\nObject %s deleted successfully." % file_name
-
-    # To delete a container. Note: The container must be empty!
-    conn.delete_container(container_name)
-    print "\nContainer %s deleted successfully.\n" % container_name
-
-httpd = Server(("", PORT), Handler)
+# Initialize the application client.
 try:
-  print("Start serving at port %i" % PORT)
-  swift()
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+	appOptions = {"org": organization, "id": appId, "auth-method": authMethod, "auth-token": authToken}
+	appCli = ibmiotf.application.Client(appOptions)
+except Exception as e:
+	print(str(e))
+	sys.exit()
+
+# Connect and configuration the application
+# - subscribe to live data from the device we created, specifically to "greeting" events
+# - use the myAppEventCallback method to process events
+appCli.connect()
+appCli.subscribeToDeviceEvents(deviceType, deviceId, "greeting")
+appCli.deviceEventCallback = myAppEventCallback
+
+# Initialize the device client.
+try:
+	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+	deviceCli = ibmiotf.device.Client(deviceOptions)
+except Exception as e:
+	print("Caught exception connecting device: %s" % str(e))
+	sys.exit()
+
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
+deviceCli.connect()
+for x in range (0,10):
+	data = { 'hello' : 'world', 'x' : x}
+	def myOnPublishCallback():
+		print("Confirmed event %s received by IoTF\n" % x)
+
+	success = deviceCli.publishEvent("greeting", "json", data, qos=0, on_publish=myOnPublishCallback)
+	if not success:
+		print("Not connected to IoTF")
+	time.sleep(1)
+
+
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
+appCli.disconnect()
