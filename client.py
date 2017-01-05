263879418bd24a74c1a42810d1c179799baac4cd
Henrik Klein <henrik.klein12@gmail.com>
2017-01-05 19:53:50 +0100

    receive and push json

---
+++ client.py
@@ -0,0 +1,94 @@
+import time
+import sys
+import pprint
+import uuid
+import serial
+from cloudant.client import Cloudant
+import couchdb
+
+#port = "/dev/cu.HC-06-DevB" # put your port name here Windows COM1/COM2/...
+#baud = 9600 # the same baud rate in your Arduino Code
+#ser = serial.Serial(port, baud, timeout=1)
+ # open the serial port
+#if ser.isOpen():
+ #print(ser.name + ' is open...')
+#while True:
+ #command = ser.readline()
+ #print command
+
+Organization ID rhq2jx
+Device Type Android
+Device ID iot3
+Authentication Method token
+Authentication Token supersecret
+
+
+
+try:
+	import ibmiotf.application
+	import ibmiotf.device
+except ImportError:
+	# This part is only required to run the sample from within the samples
+	# directory when the module itself is not installed.
+	#
+	# If you have the module installed, just use "import ibmiotf.application" & "import ibmiotf.device"
+	import os
+	import inspect
+	cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../../src")))
+	if cmd_subfolder not in sys.path:
+		sys.path.insert(0, cmd_subfolder)
+	import ibmiotf.application
+	import ibmiotf.device
+
+def myAppEventCallback(event):
+	print("Received live data from %s (%s) sent at %s: hello=%s x=%s" % (event.deviceId, event.deviceType, event.timestamp.strftime("%H:%M:%S"), data['hello'], data['x']))
+
+
+organization = "rhq2jx"
+deviceType = "Ardunio-Uno"
+deviceId = "group3"
+appId = deviceId + "_receiver"
+authMethod = "token"
+authToken = "supersecret"
+
+
+# Initialize the application client.
+try:
+	appOptions = {"org": organization, "id": appId, "auth-method": authMethod, "auth-token": authToken}
+	appCli = ibmiotf.application.Client(appOptions)
+except Exception as e:
+	print(str(e))
+	sys.exit()
+
+# Connect and configuration the application
+# - subscribe to live data from the device we created, specifically to "greeting" events
+# - use the myAppEventCallback method to process events
+appCli.connect()
+appCli.subscribeToDeviceEvents(deviceType, deviceId, "greeting")
+appCli.deviceEventCallback = myAppEventCallback
+
+# Initialize the device client.
+try:
+	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+	deviceCli = ibmiotf.device.Client(deviceOptions)
+except Exception as e:
+	print("Caught exception connecting device: %s" % str(e))
+	sys.exit()
+
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
+deviceCli.connect()
+print("---------Connected---------")
+for x in range (0,20):
+	data = { 'hello' : 'up', 'x' : x}
+	def myOnPublishCallback():
+		print("Confirmed event %s received by IoTF\n" % x)
+
+	success = deviceCli.publishEvent("greeting", "json", data, qos=0, on_publish=myOnPublishCallback)
+	if not success:
+		print("Not connected to IoTF")
+	time.sleep(1)
+
+
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
+appCli.disconnect()
--- server.py
+++ server.py
@@ -1,93 +1,133 @@
+import getopt
+import signal
 import time
 import sys
-import pprint
-import uuid
-import serial
-
-port = "/dev/cu.usbmodem1411" # put your port name here Windows COM1/COM2/...
-baud = 9600 # the same baud rate in your Arduino Code
-ser = serial.Serial(port, baud, timeout=1)
- # open the serial port
-if ser.isOpen():
- print(ser.name + ' is open...')
-while True:
- command = ser.readline()
- print command
-
-
-
+import json
 
 try:
 	import ibmiotf.application
-	import ibmiotf.device
 except ImportError:
 	# This part is only required to run the sample from within the samples
 	# directory when the module itself is not installed.
 	#
-	# If you have the module installed, just use "import ibmiotf.application" & "import ibmiotf.device"
+	# If you have the module installed, just use "import ibmiotf"
 	import os
 	import inspect
 	cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../../src")))
 	if cmd_subfolder not in sys.path:
 		sys.path.insert(0, cmd_subfolder)
 	import ibmiotf.application
-	import ibmiotf.device
 
 
-def myAppEventCallback(event):
-	print("Received live data from %s (%s) sent at %s: hello=%s x=%s" % (event.deviceId, event.deviceType, event.timestamp.strftime("%H:%M:%S"), data['hello'], data['x']))
+tableRowTemplate = "%-33s%-30s%s"
+
+def myEventCallback(event):
+	print("%-33s%-30s%s" % (event.timestamp.isoformat(), event.device, event.event + ": " + json.dumps(event.data)))
 
 
-organization = "rhq2jx"
-deviceType = "Ardunio-Uno"
-deviceId = "group3"
-appId = deviceId + "_receiver"
-authMethod = "token"
-authToken = "supersecret"
-
-#Organization ID 5xls0r
-#Device Type Ardunio-Uno
-#Device ID group3
-#Authentication Method token
-#Authentication Token WaeOU9xNtNgn4jyJ7x
-
-# Initialize the application client.
-try:
-	appOptions = {"org": organization, "id": appId, "auth-method": authMethod, "auth-token": authToken}
-	appCli = ibmiotf.application.Client(appOptions)
-except Exception as e:
-	print(str(e))
-	sys.exit()
-
-# Connect and configuration the application
-# - subscribe to live data from the device we created, specifically to "greeting" events
-# - use the myAppEventCallback method to process events
-appCli.connect()
-appCli.subscribeToDeviceEvents(deviceType, deviceId, "greeting")
-appCli.deviceEventCallback = myAppEventCallback
-
-# Initialize the device client.
-try:
-	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
-	deviceCli = ibmiotf.device.Client(deviceOptions)
-except Exception as e:
-	print("Caught exception connecting device: %s" % str(e))
-	sys.exit()
-
-# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
-deviceCli.connect()
-print("---------Connected---------")
-for x in range (0,10):
-	data = { 'hello' : 'world', 'x' : x}
-	def myOnPublishCallback():
-		print("Confirmed event %s received by IoTF\n" % x)
-
-	success = deviceCli.publishEvent("greeting", "json", data, qos=0, on_publish=myOnPublishCallback)
-	if not success:
-		print("Not connected to IoTF")
-	time.sleep(1)
+def myStatusCallback(status):
+	if status.action == "Disconnect":
+		print(tableRowTemplate % (status.time.isoformat(), status.device, status.action + " " + status.clientAddr + " (" + status.reason + ")"))
+	else:
+		print(tableRowTemplate % (status.time.isoformat(), status.device, status.action + " " + status.clientAddr))
 
 
-# Disconnect the device and application from the cloud
-deviceCli.disconnect()
-appCli.disconnect()
+def interruptHandler(signal, frame):
+	client.disconnect()
+	sys.exit(0)
+
+
+def usage():
+	print(
+		"simpleApp: Basic application connected to the IBM Internet of Things Cloud service." + "\n" +
+		"\n" +
+		"Options: " + "\n" +
+		"  -h, --help          Display help information" + "\n" +
+		"  -o, --organization  Connect to the specified organization" + "\n" +
+		"  -i, --id            Application identifier (must be unique within the organization)" + "\n" +
+		"  -k, --key           API key" + "\n" +
+		"  -t, --token         Authentication token for the API key specified" + "\n" +
+		"  -c, --config        Load application configuration file (ignore -o, -i, -k, -t options)" + "\n" +
+		"  -T, --devicetype    Restrict subscription to events from devices of the specified type" + "\n" +
+		"  -I, --deviceid      Restrict subscription to events from devices of the specified id" + "\n" +
+		"  -E, --event         Restrict subscription to a specific event"
+	)
+
+
+if __name__ == "__main__":
+	signal.signal(signal.SIGINT, interruptHandler)
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "h:o:i:k:t:c:T:I:E:", ["help", "org=", "id=", "key=", "token=", "config=", "devicetype", "deviceid", "event"])
+	except getopt.GetoptError as err:
+		print(str(err))
+		usage()
+		sys.exit(2)
+#token = JLwUBDeZt2r2&k2ax!
+	organization = "rhq2jx"
+	appId = "group3_receiver"
+	authMethod = "token"
+	authKey = "a-rhq2jx-rd8xc6cow6"
+	authToken = "2XWSfo-ZG5lzD3268v"
+	configFilePath = None
+	deviceType = "+"
+	deviceId = "+"
+	event = "+"
+
+	for o, a in opts:
+		if o in ("-o", "--organizatoin"):
+			organization = a
+		elif o in ("-i", "--id"):
+			appId = a
+		elif o in ("-k", "--key"):
+			authMethod = "apikey"
+			authKey = a
+		elif o in ("-t", "--token"):
+			authToken = a
+		elif o in ("-c", "--cfg"):
+			configFilePath = a
+		elif o in ("-T", "--devicetype"):
+			deviceType = a
+		elif o in ("-I", "--deviceid"):
+			deviceId = a
+		elif o in ("-E", "--event"):
+			event = a
+		elif o in ("-h", "--help"):
+			usage()
+			sys.exit()
+		else:
+			assert False, "unhandled option" + o
+
+	client = None
+	if configFilePath is not None:
+		options = ibmiotf.application.ParseConfigFile(configFilePath)
+	else:
+		options = {"org": organization, "id": appId, "auth-method": authMethod, "auth-key": authKey, "auth-token": authToken}
+	try:
+		client = ibmiotf.application.Client(options)
+		client.connect()
+	except ibmiotf.ConfigurationException as e:
+		print(str(e))
+		sys.exit()
+	except ibmiotf.UnsupportedAuthenticationMethod as e:
+		print(str(e))
+		sys.exit()
+	except ibmiotf.ConnectionException as e:
+		print(str(e))
+		sys.exit()
+
+
+	print("(Press Ctrl+C to disconnect)")
+
+	client.deviceEventCallback = myEventCallback
+	client.deviceStatusCallback = myStatusCallback
+
+	client.subscribeToDeviceEvents(deviceType, deviceId, event)
+	client.subscribeToDeviceStatus(deviceType, deviceId)
+
+	print("=============================================================================")
+	print(tableRowTemplate % ("Timestamp", "Device", "Event"))
+	print("=============================================================================")
+
+	while True:
+		time.sleep(5)
